ed332e17eca9419c5d8db9ba6d502a6cdded81a0
Kenji Hirabayashi <mkhira2@gmail.com>
2017-07-06 15:44:15 -0500

    arrays

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -2,30 +2,29 @@
 
 namespace CSharpFundamentals
 {
-    public class Person
-    {
-        public string FirstName;
-        public string LastName;
-
-        public void Introduce()
-        {
-            Console.WriteLine("My name is " + FirstName + " " + LastName);
-        }
-    }
-
     class Program
     {
         static void Main(string[] args)
         {
-            var john = new Person();
-            john.FirstName = "John";
-            john.LastName = "Smith";
-            john.Introduce();
+            var numbers = new int[3];
+            numbers[0] = 1;
 
-			var stacy = new Person();
-            stacy.FirstName = "Stacy";
-            stacy.LastName = "Jones";
-            stacy.Introduce();
-		}
+            Console.WriteLine(numbers[0]);
+            Console.WriteLine(numbers[1]);
+            Console.WriteLine(numbers[2]);
+
+            var flags = new bool[3];
+            flags[0] = true;
+
+            Console.WriteLine(flags[0]);
+            Console.WriteLine(flags[1]);
+            Console.WriteLine(flags[2]);
+
+            var names = new string[3] { "Jack", "John", "Mary" };
+
+            Console.WriteLine(names[0]);
+            Console.WriteLine(names[1]);
+            Console.WriteLine(names[2]);
+        }
     }
 }
