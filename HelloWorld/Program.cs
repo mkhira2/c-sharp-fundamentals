bbfe04c1bc4168d64457c13927d038cd9f71acf3
Kenji Hirabayashi <mkhira2@gmail.com>
2017-07-07 12:43:33 -0500

    for vs foreach loop

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -7,16 +7,15 @@ namespace Conditionals
     {
         static void Main(string[] args)
         {
-            for (var i = 1; i <= 10; i++) {
-                if (i % 2 == 0) {
-                    Console.WriteLine(i);
-                }
-            }
+            var name = "John Smith";
+            //for (var i = 0; i < name.Length; i++)
+            //{
+            //    Console.WriteLine(name[i]);
+            //}
 
-            for (var i = 10; i >= 1; i--) {
-                if (i % 2 == 0) {
-                    Console.WriteLine(i);
-                }
+            foreach (var character in name)
+            {
+                Console.WriteLine(character);
             }
         }
     }
