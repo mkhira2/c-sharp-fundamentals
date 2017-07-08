98d7cccb94d3765ce65517bb0692fbd3336312ad
Kenji Hirabayashi <mkhira2@gmail.com>
2017-07-08 18:24:37 -0500

    array methods

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -7,17 +7,47 @@ namespace Conditionals
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter a number: ");
-            var input = Console.ReadLine();
-            int value;
+            var numbers = new[] { 3, 7, 9, 2, 14, 6 };
 
-            if (int.TryParse(input, out value))
-            {
-                Console.WriteLine(input);
-            }
-            else {
-                Console.WriteLine("That's not a number!");
-            }
+            // Length
+
+            Console.WriteLine("Length: " + numbers.Length);
+
+            // IndexOf()
+
+            var index = Array.IndexOf(numbers, 9);
+            Console.WriteLine("Index of 9: " + index);
+
+            // Clear()
+
+            Array.Clear(numbers, 0, 2);
+            foreach (var n in numbers)
+                Console.WriteLine(n);
+
+            // Copy()
+
+            var another = new int[3];
+            Array.Copy(numbers, another, 3);
+
+            Console.WriteLine("Effects of Copy()");
+            foreach (var n in another)
+                Console.WriteLine(n);
+
+            // Sort()
+
+            Array.Sort(numbers);
+
+            Console.WriteLine("Effect of Sort()");
+            foreach (var n in numbers)
+                Console.WriteLine(n);
+
+            // Reverse()
+
+            Array.Reverse(numbers);
+
+            Console.WriteLine("Effect of Reverse()");
+            foreach (var n in numbers)
+                Console.WriteLine(n);
         }
     }
 }
