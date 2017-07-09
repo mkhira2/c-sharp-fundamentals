12672e6c39b9ecfe790ab2be8426891a417c4b1a
Kenji Hirabayashi <mkhira2@gmail.com>
2017-07-08 20:13:58 -0500

    timespan

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,24 +1,41 @@
 ﻿using System;
 
-namespace Conditionals
+namespace CSharpFundamentals
 {
 
     class Program
     {
         static void Main(string[] args)
         {
-            var dateTime = new DateTime(2015, 1, 1);
-            var now = DateTime.Now;
-            var today = DateTime.Today;
+            // Creating
 
-            Console.WriteLine("Hour: " + now.Hour);
-            Console.WriteLine("Minute: " + now.Minute);
+            var timeSpan = new TimeSpan(1, 2, 3);
 
-            var tomorrow = now.AddDays(1);
-            var yesterday = now.AddDays(-1);
+            var timeSpan1 = new TimeSpan(1, 0, 0);
+            var timeSpan2 = TimeSpan.FromHours(1);
 
-            Console.WriteLine(now.ToString("d"));
-            Console.WriteLine(now.ToString("HH:mm"));
+            var start = DateTime.Now;
+            var end = DateTime.Now.AddMinutes(2);
+            var duration = end - start;
+            Console.WriteLine("Duration: " + duration);
+
+            // Properties
+
+            Console.WriteLine("Minutes: " + timeSpan.Minutes);
+            Console.WriteLine("Total Minutes: " + timeSpan.TotalMinutes);
+
+            // Add 
+
+            Console.WriteLine("Add Example: " + timeSpan.Add(TimeSpan.FromMinutes(8)));
+            Console.WriteLine("Subtract Example: " + timeSpan.Subtract(TimeSpan.FromMinutes(8)));
+
+            // ToString
+
+            Console.WriteLine("ToString :" + timeSpan.ToString());
+
+            // Parse
+
+            Console.WriteLine("Parse: " + TimeSpan.Parse("01:02:03"));
         }
     }
 }
