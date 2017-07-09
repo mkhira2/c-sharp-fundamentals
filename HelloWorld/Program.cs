dfd979e26cdf6c42c72acdf40d94403dbbf1c22b
Kenji Hirabayashi <mkhira2@gmail.com>
2017-07-08 19:07:31 -0500

    list methods

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Conditionals
 {
@@ -7,47 +8,29 @@ namespace Conditionals
     {
         static void Main(string[] args)
         {
-            var numbers = new[] { 3, 7, 9, 2, 14, 6 };
+            var numbers = new List<int>() { 1, 2, 3, 4 };
+            numbers.Add(1);
+            numbers.AddRange(new int[3]  {5, 6, 7});
 
-            // Length
+            foreach (var number in numbers)
+                Console.WriteLine(number);
 
-            Console.WriteLine("Length: " + numbers.Length);
+            Console.WriteLine();
+            Console.WriteLine("Index of 1: " + numbers.IndexOf(1));
+            Console.WriteLine("Last Index of 1: " + numbers.LastIndexOf(1));
 
-            // IndexOf()
+            Console.WriteLine("Count: " + numbers.Count);
 
-            var index = Array.IndexOf(numbers, 9);
-            Console.WriteLine("Index of 9: " + index);
+            for (var i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] == 1)
+                    numbers.Remove(numbers[i]);
+            }
+            foreach (var number in numbers)
+                Console.WriteLine(number);
 
-            // Clear()
-
-            Array.Clear(numbers, 0, 2);
-            foreach (var n in numbers)
-                Console.WriteLine(n);
-
-            // Copy()
-
-            var another = new int[3];
-            Array.Copy(numbers, another, 3);
-
-            Console.WriteLine("Effects of Copy()");
-            foreach (var n in another)
-                Console.WriteLine(n);
-
-            // Sort()
-
-            Array.Sort(numbers);
-
-            Console.WriteLine("Effect of Sort()");
-            foreach (var n in numbers)
-                Console.WriteLine(n);
-
-            // Reverse()
-
-            Array.Reverse(numbers);
-
-            Console.WriteLine("Effect of Reverse()");
-            foreach (var n in numbers)
-                Console.WriteLine(n);
+            numbers.Clear();
+            Console.WriteLine("Count: " + numbers.Count);
         }
     }
 }
